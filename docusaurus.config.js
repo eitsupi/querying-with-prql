45ecd2554d59a85dea5af7f632841304d42f1d88
eitsupi <ts1s1andn@gmail.com>
2023-04-09 09:18:50 +0000

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,107 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Quering Book',
+  tagline: '',
+  // favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://eitsupi.github.io/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/querying-with-prql/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'eitsupi', // Usually your GitHub org/user name.
+  projectName: 'querying-with-prql', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          // editUrl: 'https://github.com/eitsupi/querying-with-prql/tree/main/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      // image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Quering with PRQL',
+        // logo: {
+        //   alt: 'Logo',
+        //   src: 'img/logo.svg',
+        // },
+        items: [
+          {
+            href: 'https://github.com/eitsupi/querying-with-prql',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'PRQL',
+            items: [
+              {
+                label: 'Website',
+                href: 'https://prql-lang.org/',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/PRQL/prql',
+              },
+              {
+                label: 'Language book',
+                href: 'https://prql-lang.org/book/',
+              },
+              {
+                label: 'Playground',
+                href: 'https://prql-lang.org/playground/',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} @eitsupi. Built with Quarto and Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['r', 'elm',],
+      },
+    }),
+};
+
+module.exports = config;
